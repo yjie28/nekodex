39b53010bd6b1f5aef05c61400324d7149bf37e1
yjie28 <xieyy28@gmail.com>
2021-01-21 18:43:53 -0800

    notes on methods & binding

--- src/App.js
+++ src/App.js
@@ -15,6 +15,17 @@ class App extends Component {
       cats: [],
       searchField: '',
     };
+
+    /*  define 'this' in methods in the constructor
+        
+        function.bind is a mehtod on any function that returns a new 
+        function where the context of 'this' is set to whatever we pass to it
+        
+        in this case, the context of 'this' is set to the 'this' keyword 
+        that was defined in the constructor
+    */
+
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -22,6 +33,23 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ cats: users }));
   }
+
+  /*  setState is undefined, because JS by default doesn't set its scope of 'this' on functions,
+      have to explicitly state what contect 'this' should be */
+
+  /* handleChange(e) {
+    this.setState({ searchField: e.target.value });
+  } */
+
+  /*  arrow functions automatically allow set 'this' when the method was defined, 
+      lexical scoping - binds the context to the place where they were defined in the first place
+      
+      When JS defined App Component, it also defined all of the methods on the components, 
+      including the handleChange because it is an arrow function
+  */
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
     const { cats, searchField } = this.state;
@@ -33,7 +61,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder="Search Cats..."
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList cats={filteredCats} />
       </div>
@@ -44,21 +72,25 @@ class App extends Component {
 export default App;
 
 /*
- *  state change -> re-render,
+ *  _state change -> re-render,
  *  whatever is inside {} is JS expression
  *
- *  state vs. props
+ *  _state vs. props
  *  state becomes prop once it gets passed down to a coponent
  *
  *  this.setState is an asynchronous function,
  *  if we want to do something with the state immediately after we set it,
  *  then we have to include a second argument
  *
- *  SetState should NOT be called in render,
+ *  _SetState should NOT be called in render,
  *  if state changes, App re-renders, and setState gets called again,
  *  this will create a loop
  *
- *  functional component - no state (no access to the constructor) or life cycle methods
+ *  _functional component - no state (no access to the constructor) or life cycle methods
  *  just returns some HTML
+ *
+ *  _Writing methods on -classes-
+ *  'this' keyword is set to the context of our class component,
+ *  'this' keyword gets bound differently depending on how we write the class method
  *
  */
